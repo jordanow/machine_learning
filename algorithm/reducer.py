85e274dfe2de59acf0b65b42356291f90c4a7b6d
Shailendra Pal <shailendrapal100@gmail.com>
2017-04-29 20:42:50 +1000

    Reduce the number of features to 200

--- algorithm/reducer.py
+++ algorithm/reducer.py
@@ -7,9 +7,12 @@ from sklearn.decomposition import IncrementalPCA as IPCA
 # size of the data we're dealing with. It is a much more memory efficient
 # method of dimensional reduction than PCA from the same module.
 
+# Only want to keep the top 200 features
+number_of_features = 200
+batch_size = 500
 
 # dimensionally reduce the given matrix
 def dimensional_reduction(matrix):
-  ipca = IPCA(n_components=200, batch_size = 5000)
+  ipca = IPCA(n_components=number_of_features, batch_size = batch_size)
   ipca.fit(matrix)
   return ipca.transform(matrix);
